dfa817d52b1015cad81b331cee363b2091978bca
mylenamelsilva <mylenamsfarias@gmail.com>
2022-06-06 21:24:30 -0300

    Desenvolvimento da função getComputerChoice

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ const getUserChoice = (userInput) => {
 // console.log(getUserChoice('papel'));
 // console.log(getUserChoice('TESOURA'));
 // console.log(getUserChoice('qualquerum'));
+
+const getComputerChoice = () => {
+    const computerAnswer = Math.floor(Math.random() * 3);
+
+    switch (computerAnswer) {
+        case 0:
+            return `pedra`;
+            break;
+        case 1:
+            return `papel`;
+            break;
+        case 2:
+            return `tesoura`;
+            break;
+        default:
+            return 'ERRO: Escolha pedra, papel ou tesoura';
+            break;
+    }
+}
+
+// console.log(getComputerChoice())
